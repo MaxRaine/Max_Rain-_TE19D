b38b8229850cb22840e0066121bc4d4d159d6eed
MaxRaine <max.raine@elev.ga.ntig.se>
2020-08-31 10:34:28 +0200

    variabler

---
+++ variabler/variabler.py
@@ -0,0 +1,11 @@
+name = "Max" # har skapat variabeln name har tilldelat den "Max"
+age = 17 # skapat variabeln age och tilldelat den "17"
+print(f"Hej {name} du är {age} år gammal")
+
+side = float(input("Ange kvadratens sida:"))
+
+area = side**2 
+
+omkrets = 4*side
+
+print(f"Kvadratens area är {area} a.e. och kvadratens omkrets är {omkrets} l.e.")
