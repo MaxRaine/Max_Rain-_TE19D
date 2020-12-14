78941b925ff47bee43e10b21e660a33f704cf21e
MaxRaine <max.raine@elev.ga.ntig.se>
2020-12-14 10:48:00 +0100

    ye

--- programmering/Blackjack/kortlek.py
+++ programmering/Blackjack/kortlek.py
@@ -1,4 +1,15 @@
+import random as rnd
+
 # behöver en lista för att hålla våra kort
+def skapaKortlek():
+    kortNummer = [i for i in range(2, 11)]
+    klädkort = ["J", "Q", "K", "A"]
+    kortNummer += klädkort
+    lek = 4*kortNummer
+    blandaKort(lek)
+
+    return lek
 
 # behöver blanda korten
-
+def blandaKort(lek):
+    return rnd.shuffle(lek)
