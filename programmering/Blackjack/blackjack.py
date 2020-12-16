18658bfc4f826b99bdf586cd9b175fa1a9845655
MaxRaine <max.raine@elev.ga.ntig.se>
2020-12-16 11:47:22 +0100

    ye

--- programmering/Blackjack/blackjack.py
+++ programmering/Blackjack/blackjack.py
@@ -1,6 +1,18 @@
 import kortlek
 
 # behöver räkna ut värden på korten
+def räknaPoäng(hand):
+    poäng = 0
+    for kort in hand:
+        if kort == "J" or kort == "Q" or kort == "K":
+            poäng += 10
+        elif kort == "A" and poäng < 11:
+            poäng += 11
+        elif kort == "A" and poäng >= 11:
+            poäng += 1
+        else:
+            poäng += kort
+    return poäng
 
 # skriv handen
 def skrivUtHanden(hand):
@@ -9,6 +21,20 @@ def skrivUtHanden(hand):
         print(str(kort) + ", ", end="")
 
 # checka vinnare
+def checkaVinnare(hand, dealer):
+    dealerPoäng = räknaPoäng(dealer)
+    spelarePoäng = räknaPoäng(hand)
+
+    print(f"Dealerns kort är {dealer[0]} och {dealer[1]}")
+    print(f"Dealerns totala poäng är {dealerPoäng}")
+    print(f"Din totala poäng är {spelarePoäng}")
+
+    if spelarePoäng == 21:
+        print("Blackjack, du vinner!")
+    elif spelarePoäng <= dealerPoäng or spelarePoäng > 21:
+        print("Dealern tar dina pengar")
+    else:
+        print("Du vinner")
 
 # spel-loop
 while True:
@@ -32,7 +58,7 @@ while True:
     while fortsätt:
         # fråga användaren om hen vill ta ett kort
         taKort = input(
-            "Ta nytt kort? (j för ja, annan tangent flr att stanna")
+            "Ta nytt kort? (j för ja, annan tangent för att stanna)")
         if taKort == "j":
             #dela ut ett kort
             hand.append(lek.pop(0))
@@ -42,6 +68,6 @@ while True:
         else:
             fortsätt = False
 
+    checkaVinnare(hand, dealer)
 
-
-
+print("Spelet slutar har en bra dag")
